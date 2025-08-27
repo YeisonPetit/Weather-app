0a7180588df66f493f1f82de4d3951c47ff3dfd6
YeisonPetit <yeisonpetit2@gmail.com>
2025-08-26 21:43:33 -0400

    Clean repository - removed exposed API key

---
+++ main.js
@@ -0,0 +1,42 @@
+const search = document.getElementById("input-search")
+const weatherContent = document.getElementById("weather-content")
+const weatherName = document.getElementById("weather-name")
+const temperature = document.getElementById("temperature")
+
+const searchCountry = async () => {
+    const apiKEY = 'b9a24ac376cf2e2e17163d15018594e5'
+    const city = search.value.trim()
+    
+    if (!city) {
+        weatherContent.innerHTML = `<h2>Please enter a city name</h2>`
+        return
+    }
+
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKEY}&units=metric`
+    
+    try {
+        const response = await fetch(url)
+        if (!response.ok) {
+            throw new Error("City not found, try again or search for other")
+        }
+        
+        const data = await response.json()
+        console.log(data)
+        
+        // Extraer datos de la respuesta de la API
+        const { name } = data
+        const { temp } = data.main
+        const { icon, description } = data.weather[0]
+
+        //insertar los datos ya almacenados al html
+        weatherContent.innerHTML = `
+            <img src="https://openweathermap.org/img/wn/${icon}.png" alt="${description}" />
+            <h1 id="weather-name">${name}</h1>
+            <span id="temperature">${temp}&#176;C</span>
+            <p>${description}</p>
+        `
+    } catch (error) {
+        weatherContent.innerHTML = `<h2>${error.message}</h2>`
+        alert(error.message)
+    }
+}
